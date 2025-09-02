0fe14b61d41f3d2226882d3fc3f956c4a705f55c
TakahiroShiota <lightright7817@gmail.com>
2025-09-02 12:07:33 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Study Planner</h1>
+      <p>ここにカレンダー / Todoリスト / タイマー機能が入ります。</p>
+    </div>
+  )
+}
